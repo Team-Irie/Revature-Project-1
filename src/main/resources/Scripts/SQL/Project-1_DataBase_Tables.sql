6c5896e522f8ddbe058e275a725707e6fed8c19f
Ryan Sy <ryansy@Ryans-Air.fios-router.home>
2022-02-04 16:22:58 -0500

    modified create user & user_role scripts in sql file

--- src/main/resources/Scripts/SQL/Project-1_DataBase_Tables.sql
+++ src/main/resources/Scripts/SQL/Project-1_DataBase_Tables.sql
@@ -1,3 +1,18 @@
+ create table user_roles(
+	id integer primary key,
+	role varchar(10)
+)
+
+create table "user"(
+	id integer primary key,
+	username varchar(50),
+	password varchar(50),
+	first_name varchar(100),
+	last_name varchar(100),
+	email varchar(150),
+	role_id integer REFERENCES user_roles
+)
+
 create table reimbursement(
 	id integer primary key,
 	amount integer,
@@ -11,16 +26,6 @@ create table reimbursement(
 	type_id integer REFERENCES reimbursement_type
 )
 
-create table user(
-	id integer primary key,
-	username varchar2(50),
-	password varchar2(50),
-	first_name varchar2(100),
-	last_name varchar2(100),
-	email varchar2(150),
-	role_id integer REFERENCES user_roles
-)
-
 create table reimbursement_status(
 	id integer primary key,
 	status varchar2(10)
@@ -29,9 +34,4 @@ create table reimbursement_status(
 create table reimbursement_type(
 	id integer primary key,
 	type varchar2(10)
-)
-
-create table user_roles(
-	id integer primary key,
-	role varchar2(10)
 )
