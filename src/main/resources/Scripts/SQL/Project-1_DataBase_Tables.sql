a1372f818bea76dfa2da3f40709b4ef7283b77b3
DominicSieli <dominicsieli@gmail.com>
2022-02-04 15:00:30 -0700

    Update sql script and created data base tables

--- src/main/resources/Scripts/SQL/Project-1_DataBase_Tables.sql
+++ src/main/resources/Scripts/SQL/Project-1_DataBase_Tables.sql
@@ -1,37 +1,37 @@
- create table user_roles(
-	id integer primary key,
-	role varchar(10)
-)
-
-create table "user"(
-	id integer primary key,
-	username varchar(50),
-	password varchar(50),
-	first_name varchar(100),
-	last_name varchar(100),
-	email varchar(150),
-	role_id integer REFERENCES user_roles
-)
-
 create table reimbursement(
 	id integer primary key,
 	amount integer,
 	submitted timestamp,
 	resolved timestamp,
-	description varchar2(250),
-	receipt blob,
-	author integer references user,
-	resolver integer REFERENCES user,
+	description varchar(250),
+	receipt bytea,
+	author integer references users,
+	resolver integer references users,
 	status_id integer references reimbursement_status,
-	type_id integer REFERENCES reimbursement_type
+	type_id integer references reimbursement_type
+)
+
+create table users(
+	id integer primary key,
+	username varchar(50) unique,
+	password varchar(50),
+	first_name varchar(100),
+	last_name varchar(100),
+	email varchar(150) unique,
+	role_id integer references user_roles
 )
 
 create table reimbursement_status(
 	id integer primary key,
-	status varchar2(10)
+	status varchar(10)
 )
 
 create table reimbursement_type(
 	id integer primary key,
-	type varchar2(10)
+	user_type varchar(10)
+)
+
+create table user_roles(
+	id integer primary key,
+	user_role varchar(10)
 )
