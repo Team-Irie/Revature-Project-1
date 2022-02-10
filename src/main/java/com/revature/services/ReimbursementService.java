eec06c9a06a0fd4d4fcb35fe2345b262e3cbea05
Ryan Sy <ryansy@Ryans-Air.fios-router.home>
2022-02-10 12:59:09 -0500

    reimbursement service done

--- src/main/java/com/revature/services/ReimbursementService.java
+++ src/main/java/com/revature/services/ReimbursementService.java
@@ -15,7 +15,7 @@ public class ReimbursementService {
     public List<Reimbursement> readByAuthorAndStatusId(int author, int status_id) { return reimbursementDao.readByAuthorAndStatusId(author, status_id); }
 
     public boolean update(Timestamp timestamp, int author, int resolver, int status_id) { return reimbursementDao.update(timestamp, author, resolver, status_id); }
-        
+
     public List<Reimbursement> readByStatusId(int status_id) { return reimbursementDao.readByStatusId(status_id ); }
 
     public List<Reimbursement> readByAuthor(int author) { return reimbursementDao.readByAuthor(author); }
