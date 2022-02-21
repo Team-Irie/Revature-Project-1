edde353643dc4861f02ff9717ebe7fc756af50a3
Ryan Sy <ryansy@Ryans-Air.fios-router.home>
2022-02-20 21:04:34 -0500

    converted reimbursement type/status

--- src/main/resources/static/js/get-requests.js
+++ src/main/resources/static/js/get-requests.js
@@ -4,14 +4,42 @@ fetch(`${window.origin}/reimbursements`)
     console.log(data);
 
     const reimbursements = data;
-    const reimbursementsLength = reimbursements.length;
+    const reimbursementsLength = reimbursements.length
+
+    function convertType(typeID) {
+      switch (typeID) {
+        case 0:
+          return 'Lodging'
+          break;
+        case 1:
+          return 'Travel'
+          break;
+        case 2:
+          return 'Food'
+          break;
+        case 3:
+          return 'Other'
+          break;
+      }
+    }
+
+    function convertStatus(statusID) {
+      switch (statusID) {
+        case 0:
+          return 'Pending'
+          break;
+        case 1:
+          return 'Resolved'
+          break;
+      }
+    }
 
     for (let i = 0; i < reimbursementsLength; i++) {
       let amount = `<td>$${reimbursements[i].amount}</td>`;
       let submitted = `<td>${new Date(reimbursements[i].submitted)}</td>`;
-      let type = `<td>${reimbursements[i].typeID}</td>`;
+      let type = `<td>${convertType(reimbursements[i].typeID)}</td>`;
       let description = `<td>${reimbursements[i].description}</td>`;
-      let status = `<td>${reimbursements[i].statusID}</td>`;
+      let status = `<td>${convertStatus(reimbursements[i].statusID)}</td>`;
       let resolver = `<td>${reimbursements[i].resolver}</td>`;
       let resolved = `<td>${null ? 'N/A' : new Date(reimbursements[i].resolved)}</td>`;
 
